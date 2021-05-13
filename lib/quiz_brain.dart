361931987da6b5b2b22019af7405009882d7428e
Vivek <37245253+fazenecture@users.noreply.github.com>
2021-05-14 00:30:08 +0530

    Update quiz_brain.dart

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -5,9 +5,9 @@ class QuizBrain {
   int _questionNumber = 0;
 
   List<Question> _questionBank = [
-    Question('You are a bitch', true),
-    Question('Single hi marega tu!', true),
-    Question('Are you a virgin', true),
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
     Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
     Question('It is illegal to pee in the Ocean in Portugal.', true),
